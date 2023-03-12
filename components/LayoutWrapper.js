245218b086b6c7a67bdae1d82d1129613e341398
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-03-12 01:29:17 -0800

    blockchain gif

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -6,6 +6,7 @@ import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import Image from 'next/image'
 
 const LayoutWrapper = ({ children }) => {
   return (
@@ -16,7 +17,13 @@ const LayoutWrapper = ({ children }) => {
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">
-                  <Logo />
+                  {/* <Logo /> */}
+                  <Image
+                    src="/static/images/blockchain.gif"
+                    alt="My Logo"
+                    width={100}
+                    height={50}
+                  />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
