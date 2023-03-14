f82aa9f708a5cf89ad1d7878eba9014bd432c4ba
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-03-14 13:08:24 -0700

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ import { Analytics } from '@vercel/analytics/react'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-// import Analytics from '@/components/analytics'
+import GAnalytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
@@ -25,6 +25,7 @@ export default function App({ Component, pageProps }) {
         </Head>
         {isDevelopment && isSocket && <ClientReload />}
         <Analytics />
+        <GAnalytics />
         <LayoutWrapper>
           <Component {...pageProps} />
         </LayoutWrapper>
