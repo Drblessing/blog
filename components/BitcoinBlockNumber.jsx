9808142af456af1093de99dddab35d6d6cf0168b
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-03-14 19:07:20 -0700

    Faster bitcoin refresh time

--- components/BitcoinBlockNumber.jsx
+++ components/BitcoinBlockNumber.jsx
@@ -11,7 +11,7 @@ const BitcoinBlockNumber = ({ isMuted }) => {
   const audioRef = useRef(null)
   const [isMounted, setIsMounted] = useState(false)
 
-  const bitcoinBlockTime = 1000 * 60 * 5
+  const bitcoinBlockTime = 1000 * 60 * 1 // 1 minute refresh interval
   const { data: blockNumber, error } = useSWR('https://blockchain.info/q/getblockcount', fetcher, {
     refreshInterval: bitcoinBlockTime,
     dedupingInterval: bitcoinBlockTime,
