8e6fe4fac6c031b9dbb814517b31c64ab7f793e4
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-03-21 14:52:51 -0700

    spring2

--- components/Footer.js
+++ components/Footer.js
@@ -11,8 +11,8 @@ export default function Footer() {
           <SocialIcon kind="github" href={siteMetadata.github} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="substack" href={siteMetadata.substack} size="6" />
-          <SocialIcon kind="mirror" href={siteMetadata.substack} size="6" />
+          {/* <SocialIcon kind="substack" href={siteMetadata.substack} size="6" /> */}
+          {/* <SocialIcon kind="mirror" href={siteMetadata.substack} size="6" /> */}
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
