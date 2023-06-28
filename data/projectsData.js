6c5ca512c4a87bd0272a8cbe39abc77b81055d7a
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-06-28 16:47:23 -0700

    Revert "fuckup".

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,6 +5,24 @@ const projectsData = [
     imgSrc: '',
     href: 'https://hackjack.vercel.app/',
   },
+  {
+    title: 'Onchain Analysis',
+    description: `Performed onchain analysis of the Ethereum blockchain. Used Python and SQL to analyze the blockchain. Used Pandas to create visualizations of the data. Used Google Big Query and Dune to query the blockchain. Created a dune dashboard for $XEX. Linked is one of my articles.`,
+    imgSrc: 'https://raw.githubusercontent.com/Drblessing/static/main/Images/onchain-analysis.png',
+    href: '/blog/profiting-from-USDC-depegging',
+  },
+  {
+    title: 'Raps Ninja',
+    description: `Full-stack react app to save song lyrics and raps.`,
+    imgSrc: 'https://raw.githubusercontent.com/Drblessing/static/main/Images/raps-ninja.png',
+    href: 'https://supa-vacation-nu.vercel.app/',
+  },
+  {
+    title: 'Forkify',
+    description: `Pure-JS recipe app.`,
+    imgSrc: 'https://raw.githubusercontent.com/Drblessing/static/main/Images/forkify.png',
+    href: 'https://forkify.pages.dev/',
+  },
 ]
 
 export default projectsData
--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Cool stuff I've built.
+            Cool stuff I've built. All open source.
           </p>
         </div>
         <div className="container py-12">
