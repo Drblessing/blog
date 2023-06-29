bad11f527a96430d3f85bf9246f32d9638771dd1
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2023-06-28 17:12:15 -0700

    It's secret!

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,8 +2,8 @@ const projectsData = [
   {
     title: 'Hackjack',
     description: `What if you could play blackjack online with two clicks of a button? Hackjack brings blackjack to the Ethereum blockchain. No more worrying about sketchy online casinos. Hackjack is provably fair and open source.`,
-    imgSrc: '',
-    href: 'https://hackjack.vercel.app/',
+    imgSrc: 'https://raw.githubusercontent.com/Drblessing/static/main/Images/hackjack.png',
+    href: 'https://hackjack.money/',
   },
   {
     title: 'Onchain Analysis',
